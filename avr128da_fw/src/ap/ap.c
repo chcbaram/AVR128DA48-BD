86a252fec4b22db6acdc2048189de61cbf2543bb
Hancheol Cho <chcbaram@gmail.com>
2022-01-09 13:59:48 +0900

    CLI 예제 추가.

--- avr128da_fw/src/ap/ap.c
+++ avr128da_fw/src/ap/ap.c
@@ -1,10 +1,12 @@
 #include "ap.h"
 
-
+void cliTest(cli_args_t *args);
 
 void apInit(void)
 {  
-  cliOpen(_DEF_UART1, 115200);
+  cliOpen(_DEF_UART1, 115200);   
+
+  cliAdd("test", cliTest);
 }
 
 void apMain(void)
@@ -23,5 +25,32 @@ void apMain(void)
     }
     
     cliMain();
+  }
+}
+
+void cliTest(cli_args_t *args)
+{
+  bool ret = false;
+
+  if (args->argc == 1 && args->isStr(0, "show") == true)
+  {
+    cliPrintf_P(PSTR("this is show command \n"));
+    ret = true;
+  }
+
+  if (args->argc == 2 && args->isStr(0, "show") == true)
+  {
+    uint8_t number;
+
+    number = args->getData(1);
+
+    cliPrintf_P(PSTR("this is show number %d \n"), number);
+    ret = true;
+  }
+
+  if (ret != true)
+  {
+    cliPrintf_P(PSTR("test show\n"));
+    cliPrintf_P(PSTR("test show 0~10\n"));
   }
 }
