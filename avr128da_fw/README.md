3d7862e949358ac19af7a6706c33c27e9fa379e0
Hancheol Cho <chcbaram@gmail.com>
2022-06-19 22:39:14 +0900

    macOS 빌드 추가.

---
+++ avr128da_fw/README.md
@@ -0,0 +1,14 @@
+# AVR128DA_FW
+
+
+```
+cmake -S . -B build -G "MinGW Makefiles"
+```
+```
+cmake -S . -B build
+```
+
+
+```
+cmake --build build -j20
+```
--- avr128da_fw/tools/avr-gcc.cmake
+++ avr128da_fw/tools/avr-gcc.cmake
@@ -1,15 +1,35 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR AVR)
 
+if (WIN32)
 set(BINUTILS_PATH ${AVR_TOOLCHAIN_DIR})
-
 set(TOOLCHAIN_PREFIX ${AVR_TOOLCHAIN_DIR}/bin/avr-)
+else()
+set(AVR_TOOLCHAIN_DIR $ENV{HOME}/hdd/tools/avr)
+set(BINUTILS_PATH ${AVR_TOOLCHAIN_DIR})
+set(TOOLCHAIN_PREFIX ${AVR_TOOLCHAIN_DIR}/bin/avr-)
+endif()
+
+
+find_program(CMAKE_MAKE_PROGRAM
+  NAMES make
+        make.exe
+  DOC "Find a suitable make program for building under Windows/MinGW"
+  HINTS c:/MinGW-32/bin ) 
+
+
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
+if (WIN32)
 set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe" CACHE FILEPATH "C Compiler path")
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++.exe" CACHE FILEPATH "C++ Compiler path")
+else()
+set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc" CACHE FILEPATH "C Compiler path")
+set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++" CACHE FILEPATH "C++ Compiler path")
+endif()
 
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
 set(CMAKE_SIZE_UTIL ${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
